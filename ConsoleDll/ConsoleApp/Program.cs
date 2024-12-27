870ffbe6df2067e2e31dcc73f5bb12565e61e925
Nazli2 <deniznazlisahin@gmail.com>
2024-12-27 13:24:02 +0300

    ConsoleAppUygulamasi

---
+++ ConsoleDll/ConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-B2CLBK8\\SQLEXPRESS;Database=ConsoleDllDb;Integrated Security=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string dllPath = @"DllApp.dll";
+                string sqlCommand;
+
+                Console.WriteLine("1.Kayıt Listele");
+                Console.WriteLine("2.Kayıt Ekle");
+                Console.WriteLine("3.Kayıt Sil");
+                Console.WriteLine("4.Çıkış");
+                Console.Write("Seçim (1/2/3/4) :");
+                string secim = Console.ReadLine();
+                string adi, soyadi = null;
+                int tabloId = 0;
+
+                switch (Convert.ToInt32(secim))
+                {
+                    case 1:
+                        sqlCommand = "Select * From UserTable";
+                        break;
+                    case 2:
+                        Console.Write("Ad Girin :");
+                        adi = Console.ReadLine();
+                        Console.Write("Soyad Girin :");
+                        soyadi = Console.ReadLine();
+
+                        sqlCommand = "Insert Into UserTable(FirstName, LastName) Values('" + adi + "','" + soyadi + "')";
+                        break;
+                    case 3:
+                        Console.Write("Silinecek Id Girin :");
+                        tabloId = Convert.ToInt32(Console.ReadLine());
+                        sqlCommand = "Delete From UserTable Where Id = " + tabloId.ToString();
+                        break;
+
+                    default:
+                        sqlCommand = null;
+                        break;
+                }
+
+                if (sqlCommand != null)
+                {
+                    Assembly assembly = Assembly.LoadFrom(dllPath);
+                    Type myClassType = assembly.GetType("DllApp.PluginClass");
+                    object myClassInstance = Activator.CreateInstance(myClassType);
+
+                    MethodInfo myMethodWithParams = myClassType.GetMethod("Run");
+                    object[] parameters = new object[] { connection, sqlCommand };
+                    myMethodWithParams.Invoke(myClassInstance, parameters);
+                }
+            }
+            Console.WriteLine("Program sonu");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleDll/DllApp/PluginClass.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllApp
+{
+    public class PluginClass
+    {
+        public class UserTable
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        public void Run(SqlConnection cnn, String cmd)
+        {
+            using (cnn)
+            {
+                try
+                {
+                    cnn.Open();
+                    Console.WriteLine("Bağlantı başarılı!");
+
+                    if (cmd.StartsWith("select", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var users = cnn.Query<UserTable>(cmd).ToList();
+
+                        foreach (var u in users)
+                        {
+                            Console.WriteLine("Id : {0}", u.Id);
+                            Console.WriteLine("Adı : {0}", u.FirstName);
+                            Console.WriteLine("Soyadı : {0}", u.LastName);
+                            Console.WriteLine("");
+                        }
+                    }
+                    else
+                    {
+                        var rowsAffected = cnn.Execute(cmd, new DynamicParameters());
+                        Console.WriteLine("{0} adet kayıt etkilendi", rowsAffected);
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
